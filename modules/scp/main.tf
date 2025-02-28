7fca88e49512648ad036d8b77fd867f2e0e16ab7
jereilfeb32 <jereilfeb13@gmail.com>
2025-02-28 23:23:25 +0800

    Update main.tf

--- modules/scp/main.tf
+++ modules/scp/main.tf
@@ -1,7 +1,12 @@
 resource "aws_organizations_policy" "scp" {
-  for_each    = var.scp_policies
-  name        = each.value
-  type        = "SERVICE_CONTROL_POLICY"
-  content     = file("policies/scp/${each.value}.json")
-  target_ids  = module.ou_creation.*.id
+  for_each = var.scp_policies
+  name     = each.key
+  type     = "SERVICE_CONTROL_POLICY"
+  content  = file("policies/scp/${each.key}.json")
+}
+
+resource "aws_organizations_policy_attachment" "attach_scp" {
+  for_each    = aws_organizations_policy.scp
+  policy_id   = each.value.id
+  target_id   = module.ou_creation[0].id
 }
